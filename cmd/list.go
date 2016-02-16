bdec05c749242f55f987cdf13717a111b72501bd
Nick Klauer <klauer@gmail.com>
2016-02-15 19:24:21 -0600

    document subcommands and root command

--- cmd/list.go
+++ cmd/list.go
@@ -32,13 +32,10 @@ var boardActions map[string][]trello.Action
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List activities on a board",
+	Long: `List will pull all the activities for a particular
+Trello board and list them in descending order.  This is useful
+if you find yourself having to see what you've been working on`,
 	Run: func(cmd *cobra.Command, args []string) {
 		Track()
 	},
--- cmd/root.go
+++ cmd/root.go
@@ -27,13 +27,11 @@ var cfgFile string
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "trackello",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Track your Trello Boards",
+	Long: `Trackello is a command-line tool to allow you to retrieve and get
+information on your Trello boards.  This tool provides a list
+of the previous 14 days of information on a board defined in
+your 'config' file (see '--config' flag for info`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//Run: func(cmd *cobra.Command, args []string) {
