3094f43abb7f7cd481b3a9240bf6520188d81ca0
Nick Klauer <klauer@gmail.com>
2016-07-23 13:14:57 -0500

    Add sort test

--- card_test.go
+++ card_test.go
@@ -17,15 +17,18 @@ func TestSorting(t *testing.T) {
 	cards[3].stats.creates = 10
 	cards[1].stats.checkListItemUpdates = 4
 	sort.Sort(ByStatisticsCountRev(cards))
+	if !sort.IsSorted(ByStatisticsCountRev(cards)) {
+		t.Fatal("Not sorted")
+	}
 	sorted := map[int]int{
-		0: 0,
-		1: 4,
-		2: 5,
-		3: 10,
+		0: 10,
+		1: 5,
+		2: 4,
+		3: 0,
 	}
 	for i, v := range cards {
 		if v.stats.Total() != sorted[i] {
-			t.Fatal("Not sorted")
+			t.Fatalf("Error testing Sort: Expected %d, got %d", sorted[i], v.stats.Total())
 		}
 	}
 }
