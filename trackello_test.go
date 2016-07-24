a46d10fe142c37566075fe2762bc2bdee1983b63
Nick Klauer <klauer@gmail.com>
2016-07-24 16:31:36 -0500

    fix broken test
    this is why you should be using constants...smh

--- trackello_test.go
+++ trackello_test.go
@@ -1,7 +1,6 @@
 package trackello
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
@@ -16,16 +15,13 @@ func TestBoard(t *testing.T) {
 		{"550ce6ae4285507e2c51f661", false},
 		{"550ce6ae4285507e2c", true},
 	}
-	client, err := NewTrackello(os.Getenv("TRACKLLEO_TOKEN"), os.Getenv("TRACKELLO_APPKEY"))
+	client, err := NewTrackello(os.Getenv("TRACKELLO_TOKEN"), os.Getenv("TRACKELLO_APPKEY"))
 	if err != nil {
 		t.Fatal(err)
 	}
 	for _, tt := range boardTests {
 		_, e := client.Board(tt.boardId)
 		hasErr := (e != nil)
-		if hasErr {
-			fmt.Printf("Error %v\n", e)
-		}
 		if tt.hasError != hasErr {
 			t.Fatalf("Expected %t for boardID '%s', got %t", tt.hasError, tt.boardId, hasErr)
 		}
