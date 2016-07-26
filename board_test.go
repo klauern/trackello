97b43e4f05d50dcacaa02c6d002808f60de9fcf1
Nick Klauer <klauer@gmail.com>
2016-07-25 19:09:43 -0500

    test multiple boards for Board Tests

--- board_test.go
+++ board_test.go
@@ -1,16 +1,16 @@
 package trackello
 
 import (
-	"testing"
 	"os"
+	"testing"
 )
 
-func SetupTest(t *testing.T) *Board {
+func SetupTest(t *testing.T, id string) *Board {
 	c, err := NewTrackello(os.Getenv(TRACKELLO_TOKEN), os.Getenv(TRACKELLO_APPKEY))
 	if err != nil {
 		t.Fatal(err)
 	}
-	b, err := c.Board("550ce6ae4285507e2c51f661")
+	b, err := c.Board(id)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,17 +18,27 @@ func SetupTest(t *testing.T) *Board {
 }
 
 func TestPopulateList(t *testing.T) {
-	board := SetupTest(t)
-	board.PopulateLists()
+	boardIDs := []string{"550ce6ae4285507e2c51f661", "56f5588daa643d38a1c7f111", "57269197fc77edb4599f2383"}
+	for _, id := range boardIDs {
+		board := SetupTest(t, id)
+		board.PopulateLists()
+	}
 
 }
 
 func TestMapActions(t *testing.T) {
-	board := SetupTest(t)
-	board.MapActions()
+	boardIDs := []string{"550ce6ae4285507e2c51f661", "56f5588daa643d38a1c7f111", "57269197fc77edb4599f2383"}
+	for _, id := range boardIDs {
+		board := SetupTest(t, id)
+		board.MapActions()
+
+	}
 }
 
 func TestPrintActions(t *testing.T) {
-	board := SetupTest(t)
-	                  board.PrintActions()
+	boardIDs := []string{"550ce6ae4285507e2c51f661", "56f5588daa643d38a1c7f111", "57269197fc77edb4599f2383"}
+	for _, id := range boardIDs {
+		board := SetupTest(t, id)
+		board.PrintActions()
+	}
 }
