e1cc071b39d1fa02b967ad5915418d77875cd623
Nick Klauer <klauer@gmail.com>
2016-10-10 19:20:01 -0500

    fix test for non-ascii color output testing

--- statistics_test.go
+++ statistics_test.go
@@ -15,7 +15,6 @@ func TestTotalStatisticsCount(t *testing.T) {
 	}
 }
 
-
 func TestPrintStatistics(t *testing.T) {
 	stats := &Statistics{
 		comments:             10,
@@ -26,7 +25,8 @@ func TestPrintStatistics(t *testing.T) {
 
 	printed := stats.PrintStatistics()
 
-	expected := "[\x1b[96m10 +\x1b[0m\x1b[91m 10 ≡\x1b[0m\x1b[92m 10 ✓\x1b[0m\x1b[95m 10 …\x1b[0m]"
+	//expected := "[\x1b[96m10 +\x1b[0m\x1b[91m 10 ≡\x1b[0m\x1b[92m 10 ✓\x1b[0m\x1b[95m 10 …\x1b[0m]"
+	expected := "[10 + 10 ≡ 10 ✓ 10 …]"
 	if printed != expected {
 		t.Fatalf("Printed output doesn't match expected: got %s, wanted %s", printed, expected)
 	}
