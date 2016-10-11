2665343b3156a30fef19133913958786e6861829
Nick Klauer <klauer@gmail.com>
2016-10-11 18:14:43 -0500

    add a test to cover the PrintNonZeroActions() call

--- list_test.go
+++ list_test.go
@@ -8,13 +8,17 @@ import (
 	"github.com/VojtechVitek/go-trello"
 )
 
-func TestSortListByName(t *testing.T) {
-	lists := []List{
+func newTestList() []List {
+	return []List{
 		*NewList(&trello.List{Name: "Z"}),
 		*NewList(&trello.List{Name: "B"}),
 		*NewList(&trello.List{Name: "A"}),
 		*NewList(&trello.List{Name: "Q"}),
 	}
+}
+
+func TestSortListByName(t *testing.T) {
+	lists := newTestList()
 	sort.Sort(ByListName(lists))
 	if !sort.IsSorted(ByListName(lists)) {
 		t.Fatal("Error: Not Sorted")
@@ -27,5 +31,12 @@ func TestSortListByName(t *testing.T) {
 	}
 	if !reflect.DeepEqual(lists, sortedLists) {
 		t.Fatal("Lists are not equal")
+	}
+}
+
+func TestPrintNonZeroActions(t *testing.T) {
+	l := *NewList(&trello.List{Name: "test"})
+	if l.PrintNonZeroActions() != "" {
+		t.Fatalf("PrintNonZeroActions did not pass: expected %s, got %s", "", l.PrintNonZeroActions())
 	}
 }
