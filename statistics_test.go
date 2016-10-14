458a462af3e9c2596355aa8ad41f6cbca5961e1e
Nick Klauer <klauer@gmail.com>
2016-10-13 20:44:32 -0500

    test adding statistics to cards

--- statistics_test.go
+++ statistics_test.go
@@ -1,6 +1,10 @@
 package trackello
 
-import "testing"
+import (
+	"testing"
+
+	trello "github.com/VojtechVitek/go-trello"
+)
 
 func TestTotalStatisticsCount(t *testing.T) {
 	stats := &Statistics{
@@ -38,5 +42,58 @@ func TestPrintEmptyStatistics(t *testing.T) {
 	expected := "[0  + 0  ≡ 0  ✓ 0  …]"
 	if printed != expected {
 		t.Fatalf("TestPrintEmptyStatistics Failed: expected %s, got %s", expected, printed)
+	}
+}
+
+func TestAddStatistics(t *testing.T) {
+	tests := []struct {
+		statType string
+		comments int
+		updates  int
+		creates  int
+		checks   int
+	}{
+		{"commentCard", 1, 0, 0, 0},
+		{"updateCard", 0, 1, 0, 0},
+		{"deleteAttachmentFromCard", 0, 1, 0, 0},
+		{"updateList", 0, 1, 0, 0},
+		{"createCard", 0, 0, 1, 0},
+		{"addChecklistToCard", 0, 0, 1, 0},
+		{"addAttachmentToCard", 0, 0, 1, 0},
+		{"updateCheckItemStateOnCard", 0, 0, 0, 1},
+		//{"junkItemAction", 0, 0, 0, 1},
+	}
+	for _, tt := range tests {
+		card := NewCard(trello.Card{})
+		err := card.AddCalculation(trello.Action{
+			Type: tt.statType,
+		})
+		if err != nil {
+			t.Fatalf("Error when adding a calculation of type %s: %v", tt.statType, err)
+		}
+		stats := card.stats
+		if stats.checkListItemUpdates != tt.checks {
+			t.Fatalf("Error with 'checkListItemUpdates' for statType %s.  Expected %v, got %v", tt.statType, tt.checks, stats.checkListItemUpdates)
+		}
+		if stats.comments != tt.comments {
+			t.Fatalf("Error with 'comments' for statType %s.  Expected %v, got %v", tt.statType, tt.comments, stats.comments)
+		}
+		if stats.creates != tt.creates {
+			t.Fatalf("Error with 'creates' for statType %s.  Expected %v, got %v", tt.statType, tt.creates, stats.creates)
+		}
+		if stats.updates != tt.updates {
+			t.Fatalf("Error with 'updates' for statType %s.  Expected %v, got %v", tt.statType, tt.updates, stats.updates)
+		}
+	}
+}
+
+func TestErrorStatisticsCases(t *testing.T) {
+	card := NewCard(trello.Card{})
+	err := card.AddCalculation(trello.Action{Type: "JunkStatusType"})
+	if err == nil {
+		t.Fatalf("Expected an error for an unknown type. Got no error")
+	}
+	if card.stats.updates != 1 {
+		t.Fatalf("expected card statistics to have %v for updates.  Got %v instead", 1, card.stats.updates)
 	}
 }
