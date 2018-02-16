74b7aa593938f4b88e54a672b935953c13f09841
Nick Klauer <klauer@gmail.com>
2018-02-16 08:12:13 -0600

    update copyright

--- cmd/version.go
+++ cmd/version.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Nick Klauer <klauer@gmail.com>
+// Copyright © 2018 Nick Klauer <klauer@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import (
 )
 
 // TrackelloVersion is the statically defined version of this project.
-var TrackelloVersion = "0.2.3"
+var TrackelloVersion string
 
 var buildDate string
 
